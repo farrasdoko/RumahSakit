3bd5aaf9d703d6d1a561ac82e3379e1d7860b0cb
farrasdoko <farras.abiyyu.doko@gmail.com>
2018-08-25 16:38:03 +0700

    Rebasing...

---
+++ app/src/main/java/com/gmail/farrasabiyyu12/rumahsakit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.farrasabiyyu12.rumahsakit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
